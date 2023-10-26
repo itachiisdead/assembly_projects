74ee064f5f38779b5431b6d4a34c739bea47487c
hebx <hebamaher0101@gmail.com>
2023-10-26 15:08:59 +0300

    Initial commit

---
+++ assemb_sec.asm
@@ -0,0 +1,37 @@
+include irvine32.inc
+.data
+    resultMsg db "Result: %d", 0
+
+.code
+main proc
+    ; Calculate the equation
+    mov eax, 60    ; eax = 60
+    sub eax, 20    ; eax = eax - 20
+    add eax, 5     ; eax = eax + 5
+    add eax, 4     ; eax = eax + 4
+
+    ; Display the result
+    mov edx, eax
+    mov ebx, OFFSET resultMsg
+    call WriteInt
+
+    exit
+main endp
+end main
+
+
+
+include irvine32.inc
+.code
+main proc
+    ; Calculate the equation
+    mov eax, 60    ; eax = 60
+    sub eax, 20    ; eax = eax - 20
+    add eax, 5     ; eax = eax + 5
+    add eax, 4     ; eax = eax + 4
+
+    call DumpRegs
+
+    exit
+main endp
+end main
